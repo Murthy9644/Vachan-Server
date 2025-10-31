674b8998189f693995d7f4089f5003b0f578d152
Murthy9644 <bvjsmurthy90@gmail.com>
2025-11-01 00:18:17 +0530

    sending regd uns finish

--- src/ServerMain.java
+++ src/ServerMain.java
@@ -1,9 +1,13 @@
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
@@ -21,11 +25,15 @@ class FileWriter{
     public static final File database = new File("Database");
     public static final File datatable = new File("Database/table.json");
 
-    public static synchronized void dataWriter(String username, HashMap<String, String> data){
+    public static synchronized void dataSetter(String username, HashMap<String, String> data){
         if (data != null) table.put(username, data);
     }
 
-    public static synchronized String fileReader(String username){
+    public static synchronized HashMap<String, HashMap<String, String>> allDataGetter(){
+        return table;
+    }
+
+    public static synchronized String specFileReader(String username){
         HashMap<String, String> regduser = table.get(username);
 
         if (regduser != null) return regduser.get("password");
@@ -53,6 +61,25 @@ class FileWriter{
         return null;
     }
 
+    public static void fileReader(){
+
+        try{
+            HashMap<String, UserDataStructure> userdata = jsonmapper.readValue(datatable, 
+            jsonmapper.getTypeFactory().constructMapType(
+                HashMap.class, String.class, UserDataStructure.class
+            ));
+
+            for (String name: userdata.keySet()){
+                HashMap<String, String> regduser = new HashMap<>();
+                regduser.put("password", userdata.get(name).password);
+                table.put(name, regduser);
+            }
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public static void fileWriter(){
 
         try{
@@ -65,14 +92,31 @@ class FileWriter{
 
     public static void dirInit(){
         if (! database.exists()) database.mkdirs();
+
+        else fileReader();
     }
 }
 
 class ClientConnection extends Thread{
     private Scanner input;
+    private ObjectInputStream objectinput;
     private PrintWriter output;
+    private ObjectOutputStream objectoutput;
     private Socket client;
     private String username;
+
+    private void closeThings(){
+        try{
+            this.input.close();
+            this.objectinput.close();
+            this.output.close();
+            this.objectoutput.close();
+            this.client.close();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public void run(){
@@ -91,12 +135,14 @@ class ClientConnection extends Thread{
             catch (NoSuchElementException e){
                 System.out.println("Client disconnected normally: " + client.getInetAddress());
                 System.out.println((e.getMessage()));
+                this.closeThings();
 
                 break;
             }
             catch (IllegalStateException e){
                 System.out.println("Client disconnected unexpectedly: " + client.getInetAddress());
                 System.out.println(e.getMessage());
+                this.closeThings();
 
                 break;
             }
@@ -104,8 +150,8 @@ class ClientConnection extends Thread{
             System.out.println("Requested: " + request);
 
             if (request.equals("EXIT")){
-                FileWriter.dataWriter(username, clientdata.getData());
                 System.out.println("Client disconnected: " + this.client.getInetAddress());
+                this.closeThings();
 
                 break;
             }
@@ -113,7 +159,7 @@ class ClientConnection extends Thread{
             switch (request){
                 case "STOREREGISTERINFO":
 
-                if (FileWriter.fileReader(username) != null){
+                if (FileWriter.specFileReader(username) != null){
                     output.println("~ACC-EXISTS");
                     continue;
                 }
@@ -122,6 +168,7 @@ class ClientConnection extends Thread{
 
                 pswd = input.nextLine();
                 clientdata.dataSetter(username, pswd);
+                FileWriter.dataSetter(username, clientdata.getData());
                 System.out.println("Client data stored successfully: " + client.getInetAddress());
                 break;
                 
@@ -129,7 +176,7 @@ class ClientConnection extends Thread{
                 HashMap<String, String> data = clientdata.getData();
 
                 if (data == null){
-                    String getpswd = FileWriter.fileReader(username);
+                    String getpswd = FileWriter.specFileReader(username);
 
                     if (getpswd == null) output.println("~NO-ACC-FOUND");
 
@@ -139,6 +186,30 @@ class ClientConnection extends Thread{
                 else output.println(data.get("password"));
                 System.out.println("Client data sent successfully: " + client.getInetAddress());
                 break;
+
+                case "GETUSERNAMES":
+                String searchstr = input.nextLine();
+                String[] searchset = FileWriter.allDataGetter().keySet().toArray(new String[0]);
+                List<String> searchres = new ArrayList<>();
+
+                for (String str: searchset){
+
+                    if (str.toLowerCase().startsWith(searchstr.toLowerCase())){
+                        searchres.add(str);
+                    }
+                }
+
+                searchres.sort(String::compareToIgnoreCase);
+                String searchres_arr[] = searchres.toArray(new String[0]);
+
+                try{
+                    objectoutput.writeObject(searchres_arr);
+                    objectoutput.flush();
+                }
+                catch (IOException e){
+                    e.printStackTrace();
+                    output.println("~ERROR");
+                }
             }
         }
     }
@@ -146,7 +217,9 @@ class ClientConnection extends Thread{
     ClientConnection(Socket client) throws Exception{
         this.client = client;
         this.input = new Scanner(this.client.getInputStream());
+        this.objectinput = new ObjectInputStream(this.client.getInputStream());
         this.output = new PrintWriter(this.client.getOutputStream(), true);
+        this.objectoutput = new ObjectOutputStream(this.client.getOutputStream()); // Need to use flush().
     }
 }
 
